059b2ae30177903e69217bd2c5835e7e4c1954fd
Allan Larangeiras <alarangeiras@gmail.com>
2016-02-19 14:23:56 -0200

    simplificacao

--- src/main/java/br/com/allanlarangeiras/geradorcontrole/Main.java
+++ src/main/java/br/com/allanlarangeiras/geradorcontrole/Main.java
@@ -1,12 +1,20 @@
 package br.com.allanlarangeiras.geradorcontrole;
 
-import br.com.allanlarangeiras.geradorcontrole.gui.JanelaPrincipal;
+import br.com.allanlarangeiras.geradorcontrole.service.GeradorControleService;
+import br.com.allanlarangeiras.geradorcontrole.service.impl.GeradorControleServiceImpl;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new JanelaPrincipal();
-
+		String cpfCnpj = "43338235000109";
+		if (args.length > 0) {
+			cpfCnpj = args[0];
+		}
+		GeradorControleService geradorControleService = GeradorControleServiceImpl.getInstance();
+		String controle = geradorControleService.gerarCodigoControle(cpfCnpj);
+		System.out.println("CPF/CNPJ: " + cpfCnpj);
+		System.out.println("Controle: " + controle);
+		System.out.println("URL Exemplo: " + "https://wwws.bradescoseguros.com.br/INET-HistoricoApolice/filtro-historico-apolice.do?ctrl=RFfQJhGARhXtEqQDDyCvAABJUjhk&cd_cgccpf=43338235000109&id_cia=1&nome=RODOBENS+ADM+E+COR+DE+SEGS+LTDA");
 	}
 	
 }
