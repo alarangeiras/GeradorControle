1c146e794eb9841c0d7535b85166efdc830ddc4f
Allan Larangeiras <alarangeiras@gmail.com>
2016-05-24 14:49:46 -0300

    ajuste para linha de comando

--- src/main/java/br/com/allanlarangeiras/geradorcontrole/Main.java
+++ src/main/java/br/com/allanlarangeiras/geradorcontrole/Main.java
@@ -1,20 +1,26 @@
 package br.com.allanlarangeiras.geradorcontrole;
 
+import java.util.Scanner;
+
 import br.com.allanlarangeiras.geradorcontrole.service.GeradorControleService;
 import br.com.allanlarangeiras.geradorcontrole.service.impl.GeradorControleServiceImpl;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String cpfCnpj = "43338235000109";
-		if (args.length > 0) {
-			cpfCnpj = args[0];
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Entre com o CPF/CNPJ: ");
+		String cpfCnpj = scanner.nextLine();
+		if (cpfCnpj == null || cpfCnpj.isEmpty()) {
+			cpfCnpj = "43338235000109";
+			
 		}
 		GeradorControleService geradorControleService = GeradorControleServiceImpl.getInstance();
 		String controle = geradorControleService.gerarCodigoControle(cpfCnpj);
 		System.out.println("CPF/CNPJ: " + cpfCnpj);
 		System.out.println("Controle: " + controle);
-		System.out.println("URL Exemplo: " + "https://wwws.bradescoseguros.com.br/INET-HistoricoApolice/filtro-historico-apolice.do?ctrl=RFfQJhGARhXtEqQDDyCvAABJUjhk&cd_cgccpf=43338235000109&id_cia=1&nome=RODOBENS+ADM+E+COR+DE+SEGS+LTDA");
+		System.out.println("\nPressione qualquer tecla para continuar\n");
+		scanner.nextLine();
 	}
 	
 }
